911ade9b22912de833368b9fb9420a2837705802
admin <admin@admins-MacBook-Pro.local>
2022-08-22 14:12:23 -0400

    added Flat.io editor pane

--- js/script.js
+++ js/script.js
@@ -1,9 +1,39 @@
+//DOM Elements
+
 const $musicPlayer = $('#music-player');
-console.log($musicPlayer);
-let embed = new Flat.Embed($musicPlayer, {
+const $quarterButton = $('button[aria-label="Quarter (3)"]');
+const $nativeQuarter = $('#quarter');
+
+
+
+//Music Player Initialized
+const embed = new Flat.Embed($musicPlayer, {
     score: '6303886e3acb0d001314122a',
     embedParams: {
-      appId: '6303986fab00b000121d88e3',
-      controlsPosition: 'bottom',
+        appId: '6303986fab00b000121d88e3',
+        controlsPosition: 'top',
+        mode: 'edit'
     }
-  });
+});
+
+
+
+
+
+
+//   embed.getMIDI().then(function (midi) {
+    //     // MIDI file as a Uint8Array
+    //     console.log(midi);
+    //   });
+    
+    
+    
+    
+    
+    // let embed = new Flat.Embed($musicPlayer, {
+    //     score: '6303886e3acb0d001314122a',
+    //     embedParams: {
+    //       appId: '6303986fab00b000121d88e3',
+    //       controlsPosition: 'top',
+    //     }
+    //   });
